9d9872bf6b4cedc62c894f41fe0ededcb002de27
Matej Bačo <matejbaco2000@gmail.com>
2025-02-16 12:39:18 +0100

    Light theme support

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,12 @@
 <script lang="ts">
 	import '../app.css';
 	import { Toaster } from '$lib/components/ui/sonner';
+	import { ModeWatcher } from 'mode-watcher';
+	import Sun from 'lucide-svelte/icons/sun';
+	import Moon from 'lucide-svelte/icons/moon';
+
+	import { toggleMode } from 'mode-watcher';
+	import { Button } from '$lib/components/ui/button/index.js';
 
 	let { data, children } = $props();
 </script>
@@ -21,6 +27,16 @@
 			<a href="/demo" class="text-foreground transition-colors"> Demo </a>
 			<a href="/auth/login" class="text-foreground transition-colors"> Login </a>
 		{/if}
+
+		<Button on:click={toggleMode} variant="outline" class="p-0 text-xs" size="icon">
+			<Sun
+				class="h-[1rem] w-[1rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0"
+			/>
+			<Moon
+				class="absolute h-[1rem] w-[1rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100"
+			/>
+			<span class="sr-only">Toggle theme</span>
+		</Button>
 	</div>
 </nav>
 
@@ -29,3 +45,4 @@
 </main>
 
 <Toaster />
+<ModeWatcher />
